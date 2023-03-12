90fe068946e5d54f4962a4c18aacec415f198f87
Kevin Harbarth <kevin.harbarth@gmail.com>
2023-03-12 18:02:57 +0100

    modified indentation setup to only work for js javascript json

--- init.el
+++ init.el
@@ -671,21 +671,24 @@ before packages are loaded."
   (setq mouse-wheel-progressive-speed nil)
 
   ;; Indentation setup. found at https://stackoverflow.com/questions/36719386/spacemacs-set-tab-width
-  (defun my-setup-indent (n)
+  ;; (defun my-setup-indent (n)
     ;; java/c/c++
     ;; (setq c-basic-offset n)
     ;; web development
     ;; (setq coffee-tab-width n) ; coffeescript
     ;; (setq javascript-indent-level n) ; javascript-mode
-    (setq js-indent-level n) ; js-mode
+    ;; (setq js-indent-level n) ; js-mode
     ;; (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
     ;; (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
     ;; (setq web-mode-css-indent-offset n) ; web-mode, css in html file
     ;; (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
     ;; (setq css-indent-offset n) ; css-mode
-    )
-  (my-setup-indent 2) ;; set indentation level to 2
+    ;; )
+  ;; (my-setup-indent 2) ;; set indentation level to 2
 
+  ;; set js-indent-level for javascript and json
+  (setq js-indent-level 2)
+  
   ;; Stop using the minibuffer when mouse leaves it.
   ;; If the mousecursor is put somewhere else, while the minibuffer is still active,
   ;; it messes with the evil-mode controls (eg. the d key will no always delete the line...).
