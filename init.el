281d93fcee53943db92778455b3051a7b84803f3
Kevin Harbarth <kevin.harbarth@gmx.net>
2023-12-23 15:47:26 +0100

    word-wrap has been set to t.

--- init.el
+++ init.el
@@ -681,6 +681,8 @@ Put your configuration code here, except for variables that should be set
 before packages are loaded."
   ;;;; General Settings
 
+  (setq word-wrap t)
+
   ;; Disable auto hiding scroll bars, so they never show up.
   (advice-remove 'mwheel-scroll #'spacemacs//scroll-bar-show-delayed-hide)
 
