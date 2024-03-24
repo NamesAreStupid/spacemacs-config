c939c67e398669af6b2761c35d4710f401bd34d8
Kevin Harbarth <kevin.harbarth@gmx.net>
2024-03-24 10:22:37 +0100

    Now using setq-default for setting word-wrap to t

--- init.el
+++ init.el
@@ -683,7 +683,7 @@ Put your configuration code here, except for variables that should be set
 before packages are loaded."
   ;;;; General Settings
 
-  (setq word-wrap t)
+  (setq-default word-wrap t)
 
   ;; Disable auto hiding scroll bars, so they never show up.
   (advice-remove 'mwheel-scroll #'spacemacs//scroll-bar-show-delayed-hide)
